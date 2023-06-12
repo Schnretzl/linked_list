00957ea0a25f2af0506d6515f6d2600ed840b286
Wes Hendley <wrhendley@gmail.com>
2023-06-12 11:14:04 -0500

    Add code for initialize, append, prepend, and size methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,19 +1,34 @@
+require_relative 'node.rb'
+
 class LinkedList
 
   def initialize
-
+    @size = 0
+    @head = nil
+    @tail = nil
   end
 
   def append(value)
+    @num_nodes += 1
+    if @head.nil?
+      prepend(value)
+    else
+      @tail = Node.new(value)
+      Node tmp = @head
+      until tmp.next_node.nil?
+        tmp = tmp.next_node
+      end
+      tmp.next_node = Node.new(value)
 
+    end
   end
 
   def prepend(value)
-
+    @head = Node.new(value)
   end
 
   def size
-
+    @num_nodes
   end
 
   def head
@@ -43,5 +58,5 @@ class LinkedList
   def to_s
 
   end
-  
+
 end
